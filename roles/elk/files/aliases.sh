0a0dacc3d83b0cbea7af7a3cebcb54f0dc08f503
dzaharavs <Dmitrijs.Zaharovs@exigenservices.com>
2018-04-13 13:21:16 +0300

    Docker related aliases

--- roles/elk/files/aliases.sh
+++ roles/elk/files/aliases.sh
@@ -12,6 +12,14 @@ echo "alias ffind='find / -name '" >> ~/.bash_aliases && source ~/.bash_aliases
 #Adding open ports display command alias
 echo "alias ports='netstat -an | grep LISTEN'" >> ~/.bash_aliases && source ~/.bash_aliases
 
-#Docker related aliases: Stop all containers
-echo "alias dstop='docker stop $(docker ps -aq)'" >> ~/.bash_aliases && source ~/.bash_aliases
+#Docker related alias: Docker stop all containers
+echo "alias dstop='docker kill $(docker ps -q)'" >> ~/.bash_aliases && source ~/.bash_aliases
 
+#Docker related alias: Docker clean containers
+echo "alias dclean='docker rm $(docker ps -a -q)'" >> ~/.bash_aliases && source ~/.bash_aliases
+
+#Docker related alias: Docker clean images
+echo "alias dwipe='docker rmi $(docker images -q)'" >> ~/.bash_aliases && source ~/.bash_aliases
+
+#Docker related alias: Docker remove all data (slow)
+echo "alias drefresh='docker system prune --all -f'" >> ~/.bash_aliases && source ~/.bash_aliases
